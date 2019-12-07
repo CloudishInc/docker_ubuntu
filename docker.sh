2b926be6d19c8c3212f4f76009cb3be7249a3534
Cloudish Inc <58627099+CloudishInc@users.noreply.github.com>
2019-12-07 20:54:11 +0530

    create docker.sh

---
+++ docker.sh
@@ -0,0 +1,10 @@
+#This script will install docker and create a CentOS container
+
+apt update -y
+wget -q -O - https://get.docker.com/ | sh
+service docker start
+docker version
+ip a
+echo "docker is installed"
+sleep 1m
+docker run -it centos:latest /bin/bash
